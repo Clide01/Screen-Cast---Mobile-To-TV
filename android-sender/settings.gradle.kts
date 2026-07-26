2eae697051a71025f6066ce785b96780a80b5013
Clide01 <zyrelclideamiana@gmail.com>
2026-07-26 21:41:32 +0800

    Add root Gradle settings and build files to android-sender

---
+++ android-sender/settings.gradle.kts
@@ -0,0 +1,20 @@
+cat << 'EOF' > ~/AppCaster/android-sender/settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AppCaster"
+include(":app")
+EOF
+
